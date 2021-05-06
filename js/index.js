6e4097141712816961b3598760d82d54f585ba60
Larissa Lannes <larissa.lannesv@gmail.com>
2021-05-06 19:01:32 -0300

    mais alguns ajustes

---
+++ js/index.js
@@ -0,0 +1,22 @@
+$("#botao").click(function (event) {
+    event.preventDefault()
+    var dataValue = $("#date").val()
+    buscador(dataValue)
+})
+
+
+function buscador(data) {
+    const key = 'mPQ7eqFL5heszXA31hwkDwXOYUdrHPd6QZJhIdaL'
+
+    $.ajax({
+        url: `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${data}`,
+        dataType: 'json',
+        success: function (dados) {
+            document.getElementById('imagem').src = dados.url
+            document.getElementById('titulo').innerText = dados.titulo
+            document.getElementById('descricao').innerText = dados.descricao
+        }
+    })
+}
+
+
